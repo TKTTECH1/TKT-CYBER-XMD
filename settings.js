862d273aff371d952bdea5cf979bcda9f96731fa
TKTTECH1 <tafadzwakureya799@gmail.com>
2026-02-26 22:43:28 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dex Bot',
+  author: '‎',
+  botName: "𝐓𝐊𝐓-𝐂𝐘𝐁𝐄𝐑-𝐗𝐃",
+  botOwner: '𝐃𝐄𝐕 𝐓𝐀𝐅𝐀𝐃𝐙𝐖𝐀-𝐓𝐊𝐓-𝐓𝐄𝐂𝐇', // Your name
+  ownerNumber: '263781400473', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "5.0.7",
+  updateZipUrl: "https://github.com/TKTTECH1/TKT-CYBER-XMD.git",
+};
+
+module.exports = settings;
