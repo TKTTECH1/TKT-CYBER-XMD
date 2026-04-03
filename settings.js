d9f2a873ac51657f954fcee8aec527dc38b0d15f
TKTTECH1 <tafadzwakureya799@gmail.com>
2026-04-03 17:16:50 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐓𝐊𝐓-𝐂𝐘𝐁𝐄𝐑-𝐗𝐌𝐃',
+  author: '‎',
+  botName: "𝐓𝐊𝐓-𝐂𝐘𝐁𝐄𝐑-𝐗𝐌𝐃",
+  botOwner: 'DEV TAFADZWA-TKT', // Your name
+  ownerNumber: '263781400473', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/TKTTECH1/TKT-CYBER-XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
